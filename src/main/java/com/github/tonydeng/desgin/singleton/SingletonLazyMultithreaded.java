931e90204cf604f6b637ca25eeee3e20ba6697ef
Tony Deng <wolf.deng@gmail.com>
2015-10-03 21:37:27 +0800

    添加多线程场景懒单例 #8

--- src/main/java/com/github/tonydeng/desgin/singleton/SingletonLazyMultithreaded.java
+++ src/main/java/com/github/tonydeng/desgin/singleton/SingletonLazyMultithreaded.java
@@ -4,4 +4,12 @@ package com.github.tonydeng.desgin.singleton;
  * Created by tonydeng on 15/10/3.
  */
 public class SingletonLazyMultithreaded {
+    private static SingletonLazyMultithreaded sc = null;
+
+    private static synchronized SingletonLazyMultithreaded getInstance(){
+        if(sc == null){
+            sc = new SingletonLazyMultithreaded();
+        }
+        return sc;
+    }
 }
