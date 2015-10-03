c7e109d56a63d991b0d6ff27c40a19fa20c7a498
Tony Deng <wolf.deng@gmail.com>
2015-10-03 21:37:11 +0800

    添加懒单例 #8

--- src/main/java/com/github/tonydeng/desgin/singleton/SingletonLazy.java
+++ src/main/java/com/github/tonydeng/desgin/singleton/SingletonLazy.java
@@ -4,4 +4,14 @@ package com.github.tonydeng.desgin.singleton;
  * Created by tonydeng on 15/10/3.
  */
 public class SingletonLazy {
+    private static SingletonLazy sc = null;
+
+    private SingletonLazy(){}
+
+    public static SingletonLazy getInstance(){
+        if(sc == null){
+            sc = new SingletonLazy();
+        }
+        return sc;
+    }
 }
