77c0d0168cdfb5cfa088ed3580f6ac7c2bf3d2bf
Tony Deng <wolf.deng@gmail.com>
2015-10-03 21:49:30 +0800

    添加双重校验单例 #8

--- src/main/java/com/github/tonydeng/desgin/singleton/SingletonLazyDoubleCheck.java
+++ src/main/java/com/github/tonydeng/desgin/singleton/SingletonLazyDoubleCheck.java
@@ -4,4 +4,18 @@ package com.github.tonydeng.desgin.singleton;
  * Created by tonydeng on 15/10/3.
  */
 public class SingletonLazyDoubleCheck {
+    private static SingletonLazyDoubleCheck sc = null;
+
+    private SingletonLazyDoubleCheck(){}
+
+    public static SingletonLazyDoubleCheck getInstance(){
+        if(sc == null){
+            synchronized (SingletonLazyDoubleCheck.class){
+                if(sc == null){
+                    sc = new SingletonLazyDoubleCheck();
+                }
+            }
+        }
+        return sc;
+    }
 }
