b69b40a4e35df7580883231625f7d2b3f0be7b6c
Tony Deng <wolf.deng@gmail.com>
2018-09-06 20:04:49 +0800

    :bug:fix(1.0): 解决Sonar报错的问题
    refs #10
    
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

--- src/main/java/com/github/tonydeng/desgin/cor/Dollar10Dispenser.java
+++ src/main/java/com/github/tonydeng/desgin/cor/Dollar10Dispenser.java
@@ -4,26 +4,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Dollar10Dispenser implements DispenseChain {
-    private static final Logger log = LoggerFactory.getLogger(Dollar10Dispenser.class);
-    private DispenseChain chain;
+	private static final Logger log = LoggerFactory.getLogger(Dollar10Dispenser.class);
+	private DispenseChain chain;
 
-    @Override
-    public void setNextChain(DispenseChain nextChain) {
-        this.chain = nextChain;
-    }
+	@Override
+	public void setNextChain(DispenseChain nextChain) {
+		this.chain = nextChain;
+	}
 
-    @Override
-    public void dispense(Currency currency) {
-        if (currency.getAmount() >= 10) {
-            int num = currency.getAmount() / 10;
-            int remainder = currency.getAmount() % 10;
+	@Override
+	public void dispense(Currency currency) {
+		if (currency.getAmount() >= 10) {
+			int num = currency.getAmount() / 10;
+			int remainder = currency.getAmount() % 10;
 
-            log.info("Dispensing {} 10$ note");
-            if(remainder !=0 ){
-                this.chain.dispense(new Currency(remainder));
-            }
-        }else {
-            this.chain.dispense(currency);
-        }
-    }
+			log.info("Dispensing {} 10$ note", num);
+			if (remainder != 0) {
+				this.chain.dispense(new Currency(remainder));
+			}
+		} else {
+			this.chain.dispense(currency);
+		}
+	}
 }
--- src/main/java/com/github/tonydeng/desgin/cor/Dollar20Dispenser.java
+++ src/main/java/com/github/tonydeng/desgin/cor/Dollar20Dispenser.java
@@ -4,26 +4,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Dollar20Dispenser implements DispenseChain {
-    private static final Logger log = LoggerFactory.getLogger(Dollar20Dispenser.class);
-    private DispenseChain chain;
+	private static final Logger log = LoggerFactory.getLogger(Dollar20Dispenser.class);
+	private DispenseChain chain;
 
-    @Override
-    public void setNextChain(DispenseChain nextChain) {
-        this.chain = nextChain;
-    }
+	@Override
+	public void setNextChain(DispenseChain nextChain) {
+		this.chain = nextChain;
+	}
 
-    @Override
-    public void dispense(Currency currency) {
-        if (currency.getAmount() >= 20) {
-            int num = currency.getAmount() / 20;
-            int remainder = currency.getAmount() % 20;
+	@Override
+	public void dispense(Currency currency) {
+		if (currency.getAmount() >= 20) {
+			int num = currency.getAmount() / 20;
+			int remainder = currency.getAmount() % 20;
 
-            log.info("Dispensing {} 20$ note");
-            if(remainder !=0 ){
-                this.chain.dispense(new Currency(remainder));
-            }
-        }else {
-            this.chain.dispense(currency);
-        }
-    }
+			log.info("Dispensing {} 20$ note", num);
+			if (remainder != 0) {
+				this.chain.dispense(new Currency(remainder));
+			}
+		} else {
+			this.chain.dispense(currency);
+		}
+	}
 }
